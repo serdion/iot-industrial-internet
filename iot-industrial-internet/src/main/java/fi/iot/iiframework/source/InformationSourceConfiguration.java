94ef8f042cf3cd733c416db5e1b42a99432eb7f0
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-20 14:31:44 +0300

    made configuration class public

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/source/InformationSourceConfiguration.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/source/InformationSourceConfiguration.java
@@ -10,6 +10,6 @@ package fi.iot.iiframework.source;
  *
  * Configures an information source object based on database configuration information
  */
-class InformationSourceConfiguration {
+public class InformationSourceConfiguration {
 
 }
