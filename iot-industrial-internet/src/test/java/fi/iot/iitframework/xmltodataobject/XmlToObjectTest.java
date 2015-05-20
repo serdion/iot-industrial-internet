300857a56f93403f62fcf19db7805295be318104
Petri Pihlajaniemi <petri.pihlajaniemi@gmail.com>
2015-05-20 13:38:44 +0300

    test package

---
+++ iot-industrial-internet/src/test/java/fi/iot/iitframework/xmltodataobject/XmlToObjectTest.java
@@ -0,0 +1,44 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+
+package fi.iot.iitframework.xmltodataobject;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class XmlToObjectTest {
+    
+    public XmlToObjectTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+//    @Test
+//    public void testSomeMethod() {
+//        fail("The test case is a prototype.");
+//    }
+    
+}
