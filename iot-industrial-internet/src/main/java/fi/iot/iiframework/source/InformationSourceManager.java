c21b6dde12dbd3c3ef67358d958f4e9d06475873
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-20 14:28:30 +0300

    added licenses for source classes

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/source/InformationSourceManager.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/source/InformationSourceManager.java
@@ -1,3 +1,9 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
 package fi.iot.iiframework.source;
 
 import java.util.ArrayList;
