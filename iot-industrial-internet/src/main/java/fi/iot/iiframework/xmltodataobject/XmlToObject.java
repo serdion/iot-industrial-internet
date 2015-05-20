977b27f4b051ba27db5675a80aad2c5160b8b4a4
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-20 14:55:48 +0300

    small fixes

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/xmltodataobject/XmlToObject.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/xmltodataobject/XmlToObject.java
@@ -9,7 +9,6 @@ import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlRootElement;
 
 public class XmlToObject {
 
@@ -26,9 +25,7 @@ public class XmlToObject {
             JAXBContext context = JAXBContext.newInstance(DataObject.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
             data = (DataObject) unmarshaller.unmarshal(new URL(url));
-        } catch (JAXBException ex) {
-            Logger.getLogger(XmlToObject.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (MalformedURLException ex) {
+        } catch (JAXBException | MalformedURLException ex) {
             Logger.getLogger(XmlToObject.class.getName()).log(Level.SEVERE, null, ex);
         }
 
