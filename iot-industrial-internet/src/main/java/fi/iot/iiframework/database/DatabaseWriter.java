7587038768d56000ef0c7d6d8a13da34289dd0e4
Axel Wikström <axel.wikstrom@gmail.com>
2015-05-21 10:50:06 +0300

    added interfaces for db reader and writer

---
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/database/DatabaseWriter.java
@@ -0,0 +1,13 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+package fi.iot.iiframework.database;
+
+import fi.iot.iiframework.dataobject.DataObject;
+
+public interface DatabaseWriter {
+    public void writeToDb(DataObject obj);
+}
