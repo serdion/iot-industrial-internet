66cb439e3dec76907f3a31d8b982db98d7b9764c
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-21 13:36:43 +0300

    dataobjectfactorytest is finished

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/dataobject/DataObjectFactoryTest.java
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/dataobject/DataObjectFactoryTest.java
@@ -28,5 +28,26 @@ public class DataObjectFactoryTest {
         assertTrue(obj.getHeader().getUptime() > 1000 && obj.getHeader().getUptime() < 10000);
         assertEquals("success", obj.getHeader().getResponse());
     }
+    
+    @Test
+    public void devicesAreGeneratedProperly() {
+        assertEquals(10, obj.getDevices().size());
+        assertTrue(obj.getDevices().get(0).status);
+        assertNotEquals(null, obj.getDevices().get(0).id);
+    }
+    
+    @Test
+    public void sensorsAreGeneratedProperly() {
+        assertEquals(10, obj.getDevices().get(0).getSensors().size());
+        assertNotEquals(null, obj.getDevices().get(0).getSensors().get(0));
+    }
+    
+    @Test
+    public void readoutsAreGeneratedProperly() {
+        assertEquals(25, obj.getDevices().get(0).getSensors().get(0).getReadouts().size());
+        assertEquals("Temperature", obj.getDevices().get(0).getSensors().get(0).getReadouts().get(0).quantity);
+        assertEquals("C", obj.getDevices().get(0).getSensors().get(0).getReadouts().get(0).unit);
+        assertTrue(obj.getDevices().get(0).getSensors().get(0).getReadouts().get(0).value >= 22.1 && obj.getDevices().get(0).getSensors().get(0).getReadouts().get(0).value <= 23.1);
+    }
 
 }
