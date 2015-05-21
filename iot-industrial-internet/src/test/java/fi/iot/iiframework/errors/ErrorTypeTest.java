de04ea2881fe1e3644ce789c9a7c84f044353804
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-21 13:05:39 +0300

    some junit updates

---
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/errors/ErrorTypeTest.java
@@ -0,0 +1,29 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+package fi.iot.iiframework.errors;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ErrorTypeTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testValues() {
+//        System.out.println("values");
+//        ErrorType[] expResult = null;
+//        ErrorType[] result = ErrorType.values();
+//        assertArrayEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
