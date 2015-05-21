090ca03ae46d246df60b6db46656aa430ad2028c
Axel Wikström <axel.wikstrom@gmail.com>
2015-05-21 14:39:16 +0300

    working h2 in-memory database

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/database/HibernateUtil.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/database/HibernateUtil.java
@@ -1,4 +1,3 @@
-
 package fi.iot.iiframework.database;
 
 import fi.iot.iiframework.DemoSaveable;
@@ -7,21 +6,27 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
-    private static final String CONFIGFILE = "hibernate.cfg.xml";
-    
+    private static final String CONFIGFILE = "hibernate_h2.cfg.xml";
+
     private static SessionFactory buildSessionFactory() {
         try {
-            AnnotationConfiguration configuration = new AnnotationConfiguration().configure(CONFIGFILE);
-            configuration.addAnnotatedClass(DemoSaveable.class);
-            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-            return configuration.buildSessionFactory(serviceRegistry);
-        }
-        catch (Throwable ex) {
+//            AnnotationConfiguration configuration = new AnnotationConfiguration().configure(CONFIGFILE);
+//            configuration.addAnnotatedClass(DemoSaveable.class);
+//            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+//                    .applySettings(configuration.getProperties()).build();
+//            return configuration.buildSessionFactory(serviceRegistry);
+            SessionFactory sessionFactory = new Configuration().configure(CONFIGFILE)
+                    .addAnnotatedClass(DemoSaveable.class)
+                    .buildSessionFactory();
+
+            return sessionFactory;
+
+        } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
