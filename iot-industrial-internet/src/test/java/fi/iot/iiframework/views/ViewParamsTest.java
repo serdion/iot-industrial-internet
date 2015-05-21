f4b243b8ea37d6032bc6746e51bb048c8757b992
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-21 14:18:15 +0300

    small fixes

---
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/views/ViewParamsTest.java
@@ -0,0 +1,23 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+package fi.iot.iiframework.views;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ViewParamsTest {
+
+    @Test
+    public void testSetAllTypes() {
+        System.out.println("setAllTypes");
+        String type = "";
+        ViewParams instance = new ViewParams();
+        instance.setAllTypes(type);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
