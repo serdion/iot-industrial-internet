bd18bee2338ea0fb6f7338c3e5d2e94d676ced1c
Petri <Petri.pihlajaniemi@gmail.com>
2015-05-22 12:13:05 +0300

    Error mapping ja dao

---
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/errors/dao/ErrorDAOImpl.java
@@ -0,0 +1,16 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+package fi.iot.iiframework.errors.dao;
+
+/**
+ *
+ * @author petri
+ */
+public class ErrorDAOImpl implements ErrorDAO {
+    
+    
+}
