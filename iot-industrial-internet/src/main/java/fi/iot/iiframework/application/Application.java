71ea3477f57d88a1ff7bece905aa53a92d129efc
Atte Lassila <atte.lassila@cs.helsinki.fi>
2015-05-22 14:35:25 +0300

    spring-context configuration

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/application/Application.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/application/Application.java
@@ -25,7 +25,7 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
-        HibernateUtil.getSessionFactory().openSession();
+        
     }
 
 }
--- iot-industrial-internet/src/main/java/fi/iot/iiframework/database/HibernateUtil.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/database/HibernateUtil.java
@@ -11,28 +11,9 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory = buildSessionFactory();
-    private static final String CONFIGFILE = "hibernate_h2.cfg.xml";
-
-    private static SessionFactory buildSessionFactory() {
-        try {
-            SessionFactory sessionFactory = new Configuration().configure(CONFIGFILE)
-                    .buildSessionFactory();
-
-            return sessionFactory;
-
-        } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
 
 }
