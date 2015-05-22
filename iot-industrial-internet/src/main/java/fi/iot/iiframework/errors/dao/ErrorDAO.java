c2bbd12fe16eaa3664492483e7b9d3656624163d
Petri <Petri.pihlajaniemi@gmail.com>
2015-05-22 12:23:45 +0300

    Delete author tags

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/errors/dao/ErrorDAO.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/errors/dao/ErrorDAO.java
@@ -6,10 +6,41 @@
  */
 package fi.iot.iiframework.errors.dao;
 
+import java.util.List;
+
 /**
+ * DAO for Error
  *
- * @author petri
  */
-interface ErrorDAO {
+public interface ErrorDAO {
     
+    /**
+     * Save error to database
+     * @param error 
+     */
+
+    public void save(Error error);
+    
+    /**
+     * Get error from database based on id
+     * @param id of the error
+     * @return 
+     */
+
+    public Error get(String id);
+    
+    /**
+     * Get a list of errors in database
+     * @return 
+     */
+
+    public List<Error> getAll();
+    
+    /**
+     * Remove an error from database
+     * @param id of the error
+     */
+
+    public void remove(String id);
+
 }
