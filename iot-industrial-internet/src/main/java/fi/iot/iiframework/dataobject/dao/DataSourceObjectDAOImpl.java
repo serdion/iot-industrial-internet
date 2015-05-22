18772606bf7d339443a5630eb7d5e27b4b79aa35
Atte Lassila <atte.lassila@cs.helsinki.fi>
2015-05-22 10:18:09 +0300

    Impl minimal change

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/dataobject/dao/DataSourceObjectDAOImpl.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/dataobject/dao/DataSourceObjectDAOImpl.java
@@ -9,30 +9,34 @@ package fi.iot.iiframework.dataobject.dao;
 import fi.iot.iiframework.database.HibernateUtil;
 import fi.iot.iiframework.dataobject.DataSourceObject;
 import java.util.List;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author atte
  */
+@Repository
 public class DataSourceObjectDAOImpl implements DataSourceObjectDAO {
 
     @Override
-    public void save(DataSourceObject dbo) {
+    public void save(DataSourceObject dso) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.persist(dbo);
+        session.persist(dso);
     }
 
     @Override
     public DataSourceObject get(String id) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        return null;
+        return (DataSourceObject) session.get(DataSourceObject.class, id);
     }
 
     @Override
     public List<DataSourceObject> getAll() {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        return null;
+        Criteria criteria = session.createCriteria(DataSourceObject.class);
+        return criteria.list();
     }
 
     @Override
