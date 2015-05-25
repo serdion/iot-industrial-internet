6dd29a4c8019828cefef29b80c0574c209febc57
Axel Wikström <axel.wikstrom@gmail.com>
2015-05-25 13:45:29 +0300

    trying to use xml

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/application/Application.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/application/Application.java
@@ -11,18 +11,38 @@ import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication
+@ComponentScan("fi.iot.iiframework.database")
+@ImportResource("classpath:spring-context.xml")
 public class Application {
     
     private static final Logger logger = Logger.getLogger(Application.class.getName());
 
+//    static HibernateConfiguration hc = new HibernateConfiguration();
+    
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
         HibernateUtil.getSessionFactory().openSession();
     }
+    
+//    @Bean
+//    HibernateTransactionManager transactionManager() {
+//        return hc.transactionManager();
+//    }
+//    
+//    @Bean
+//    DataSource dataSource() {
+//        return hc.dataSource();
+//    }
+//    
+//    @Bean
+//    LocalSessionFactoryBean sessionFactory() {
+//        return hc.sessionFactory();
+//    }
 
 }
