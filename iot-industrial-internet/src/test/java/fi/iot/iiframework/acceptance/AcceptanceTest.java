49e98b7dae4e7cb0245f598ac868773aa0432eb9
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-26 15:08:27 +0300

    finished fixing the damage to acceptance tests by the revert mess

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/AcceptanceTest.java
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/AcceptanceTest.java
@@ -10,11 +10,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+@Ignore
+// comment this @Ignore line if you want to run the acceptance tests, but make sure that the
+// server is running first by pressing Run Project!
 public class AcceptanceTest {
 
     private WebDriver webDriver;
@@ -25,27 +29,25 @@ public class AcceptanceTest {
         webDriver.get("http://localhost:8080");
     }
 
-//    @Test
-//    public void webInterfaceOpensProperly() {
-//        // all of the tests in this class now work IF you first choose Run Project and have the server running properly
-//        
-//        String title = webDriver.getTitle();
-//        assertEquals("Hello world!", title);
-//        
-//        WebElement element = webDriver.findElement(By.id("wrapper"));
-//        String tagname = element.getTagName();
-//        assertEquals("div", tagname);
-//    }
-//    
-//    @Test
-//    public void webInterfaceShowsSomeXMLData() {
-//        webDriver.get("http://localhost:8080/sources/example/view");
-//        String title = webDriver.getTitle();
-//        assertEquals("List of all Sensors", title);
-//        
-//        WebElement element = webDriver.findElement(By.linkText("Readouts"));
-//        assertNotEquals(null, element);  
-//    }
+    @Test
+    public void webInterfaceOpensProperly() {
+        String title = webDriver.getTitle();
+        assertEquals("Helo world!", title);
+        
+        WebElement element = webDriver.findElement(By.id("wrapper"));
+        String tagname = element.getTagName();
+        assertEquals("div", tagname);
+    }
+    
+    @Test
+    public void webInterfaceShowsSomeXMLData() {
+        webDriver.get("http://localhost:8080/sources/example/view");
+        String title = webDriver.getTitle();
+        assertEquals("List of all Sensors", title);
+        
+        WebElement element = webDriver.findElement(By.linkText("Readouts"));
+        assertNotEquals(null, element);  
+    }
 
     @After
     public void close() {
