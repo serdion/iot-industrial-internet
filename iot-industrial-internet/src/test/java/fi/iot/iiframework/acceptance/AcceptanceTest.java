b2ce933860dc760368b76e0667dc37a7b2c07aa6
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-26 15:21:48 +0300

    lots of miscellaneous stuff with acceptance tests

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/AcceptanceTest.java
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/AcceptanceTest.java
@@ -6,15 +6,25 @@
  */
 package fi.iot.iiframework.acceptance;
 
+import fi.iot.iiframework.application.Application;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Ignore;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes=Application.class)
+//@WebAppConfiguration
 
 @Ignore
 // comment this @Ignore line if you want to run the acceptance tests, but make sure that the
@@ -25,14 +35,15 @@ public class AcceptanceTest {
 
     @Before
     public void open() {
+//        SpringApplication.run(Application.class);
         webDriver = new HtmlUnitDriver();
-        webDriver.get("http://localhost:8080");
     }
 
     @Test
     public void webInterfaceOpensProperly() {
+        webDriver.get("http://localhost:8080");
         String title = webDriver.getTitle();
-        assertEquals("Helo world!", title);
+        assertEquals("Hello world!", title);
         
         WebElement element = webDriver.findElement(By.id("wrapper"));
         String tagname = element.getTagName();
