fff697fb70e26cfcf7995778a05487480f624ffd
Atte Lassila <atte.lassila@cs.helsinki.fi>
2015-05-27 15:50:56 +0300

    commented test class for now

--- iot-industrial-internet/src/test/java/DataSourceObjectDAOImplTest.java
+++ iot-industrial-internet/src/test/java/DataSourceObjectDAOImplTest.java
@@ -1,29 +1,29 @@
-/*
- * IoT - Industrial Internet Framework
- * Apache License Version 2.0, January 2004
- * Released as a part of Helsinki University
- * Software Engineering Lab in summer 2015
- */
-
-import fi.iot.iiframework.database.HibernateConfiguration;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = HibernateConfiguration.class)
-public class DataSourceObjectDAOImplTest {
-    
-    
-    
-    @Before
-    public void setUp() {
-    }
-    
-}
+///*
+// * IoT - Industrial Internet Framework
+// * Apache License Version 2.0, January 2004
+// * Released as a part of Helsinki University
+// * Software Engineering Lab in summer 2015
+// */
+//
+//import fi.iot.iiframework.database.HibernateConfiguration;
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = HibernateConfiguration.class)
+//public class DataSourceObjectDAOImplTest {
+//    
+//    
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//}
