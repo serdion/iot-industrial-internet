8bff490ee74f6da25cea33171f5fb3b9d8b2e5ef
Atte Lassila <atte.lassila@cs.helsinki.fi>
2015-05-27 15:47:48 +0300

    Readout getBy(sensor) implemented in service

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/services/dataobject/ReadoutService.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/services/dataobject/ReadoutService.java
@@ -7,7 +7,11 @@
 package fi.iot.iiframework.services.dataobject;
 
 import fi.iot.iiframework.dataobject.Readout;
+import fi.iot.iiframework.dataobject.Sensor;
 import fi.iot.iiframework.services.GenericService;
+import java.util.List;
 
 public interface ReadoutService extends GenericService<Readout, Long> {
+    public List<Readout> getBy(Sensor sensor);
+    public List<Readout> getBy(int from, int to, Sensor sensor);
 }
--- iot-industrial-internet/src/main/java/fi/iot/iiframework/services/dataobject/ReadoutServiceImpl.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/services/dataobject/ReadoutServiceImpl.java
@@ -8,20 +8,35 @@ package fi.iot.iiframework.services.dataobject;
 
 import fi.iot.iiframework.dataobject.Readout;
 import fi.iot.iiframework.daos.dataobject.ReadoutDAO;
+import fi.iot.iiframework.dataobject.Sensor;
 import fi.iot.iiframework.services.GenericHibernateService;
+import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
-public class ReadoutServiceImpl 
-extends GenericHibernateService<Readout, Long>
-implements ReadoutService {
+public class ReadoutServiceImpl
+        extends GenericHibernateService<Readout, Long>
+        implements ReadoutService {
+
+    ReadoutDAO readoutDAO;
     
     @Autowired
     public ReadoutServiceImpl(ReadoutDAO dao) {
+        readoutDAO = dao;
         super.dao = dao;
+    }
+  
+    @Override
+    public List<Readout> getBy(int from, int to, Sensor sensor) {
+        return readoutDAO.getBy(from, to, sensor);
+    }
+    
+    @Override
+    public List<Readout> getBy(Sensor sensor) {
+        return getBy(0, Integer.MAX_VALUE, sensor);
     }
 
 }
