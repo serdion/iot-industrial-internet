f21825aaeca507d6acc6a0f870268adf3edfa266
Petri Pihlajaniemi <petri.pihlajaniemi@gmail.com>
2015-05-28 14:21:08 +0300

    starting errorloggertests

---
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/errors/ErrorLoggerTest.java
@@ -0,0 +1,41 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+
+package fi.iot.iiframework.errors;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+
+public class ErrorLoggerTest {
+    
+    public ErrorLoggerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
