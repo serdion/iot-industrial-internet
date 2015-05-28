0833603912e34efa490342bdd4500d2bd06d4cc3
Teemu Salminen <teemujsalminen@gmail.com>
2015-05-28 15:26:51 +0300

    some changes

---
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/services/DataSourceObjectServiceTest.java
@@ -0,0 +1,42 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+package fi.iot.iiframework.services;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DataSourceObjectServiceTest {
+    
+    public DataSourceObjectServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
