2f0f4b074a8d4464b5fbe181b0d76dfb64625f79
Petri Pihlajaniemi <petri.pihlajaniemi@gmail.com>
2015-06-04 16:03:31 +0300

    reloads alertlist on click

--- iot-industrial-internet/src/main/webapp/js/syserrors/syserror_controllers.js
+++ iot-industrial-internet/src/main/webapp/js/syserrors/syserror_controllers.js
@@ -7,27 +7,27 @@
 
 // Get all syserrors in a list
 sysErrors.controller('SysErrorsListController', function($scope, SysError, $routeParams) {
-    console.log("Get all errors");
     $scope.errorlist = SysError.get(function(data) {
-        console.log(data);
+//        console.log(data);
     });
 });
 
 //Get one syserror based on id
 sysErrors.controller('SysErrorDetailsController', function($scope, SysError, $routeParams) {
-    console.log("Get single error!");
     $scope.errordetails = SysError.query({errorid: $routeParams.errorid}, function(data) {
-        console.log(data);
+//        console.log(data);
     });
 });
 
 //Get errors of severity equal or higher than high
 
 sysErrors.controller('ErrorNotificationController', function($scope, SysError, $routeParams) {
-    console.log("Get all errors to alarmlist without any filtering yet!");
     $scope.alarmlist = SysError.getHighAndFatal({errorid: $routeParams.errorid}, function(data) {
-        console.log(data);
+//        console.log(data);
     });
+    $scope.reloadErrors = function () {
+        $scope.alarmlist =  SysError.getHighAndFatal({errorid: $routeParams.errorid});
+    };
 });
 
 
