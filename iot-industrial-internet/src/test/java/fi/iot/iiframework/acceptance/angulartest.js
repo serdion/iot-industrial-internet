6aa3347ca15f80a6fc8a23fed855552f10428b93
Teemu Salminen <teemujsalminen@gmail.com>
2015-06-05 15:29:10 +0300

    almost functional protractor tests

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/angulartest.js
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/angulartest.js
@@ -10,20 +10,29 @@ describe('Acceptance testing for the AngularJS web interface', function () {
         element(by.partialLinkText('Information Sources')).click();
         expect(browser.getTitle()).toEqual('IIFramework');
     });
-
-    it('should be able create a new information source', function () {
+    
+    it('should be able view a sensor and filter its readouts', function () {
         browser.get('http://localhost:8080/#/sources/');
-        element(by.partialLinkText('Add Information source')).click();
-        var name = element(by.model("is_name"));
-        var type = element(by.model("is_type"));
-        var url = element(by.model("is_url"));
-        var frequency = element(by.model("is_freq"));
-        name.sendKeys('test');
-        type.sendKeys('XML');
-        url.sendKeys('http://axwikstr.users.cs.helsinki.fi/data.xml');
-        frequency.sendKeys('10');
-        element(by.id('roundbutton')).click();
+        element(by.partialLinkText('View')).click();
+        element(by.partialLinkText('List')).click();
+        element(by.model("more")).sendKeys("22.50");
+        element(by.model("less")).sendKeys("22.80");
+        element(by.id('filter')).click();
         expect(browser.getTitle()).toEqual('IIFramework');
     });
 
+    it('should be able create a new information source', function () {
+        browser.get('http://localhost:8080/#/sources/');
+        element(by.partialLinkText('Add New')).click();
+        var name = element(by.model("is.name"));
+        var type = element(by.model("is.type"));
+        var url = element(by.model("is.url"));
+        var frequency = element(by.model("is.readFrequency"));
+        name.sendKeys('NewAcceptanceTestSource');
+        element(by.cssContainingText('option', 'XML')).click();
+        url.sendKeys('http://axwikstr.users.cs.helsinki.fi/data.xml');
+        frequency.sendKeys('154');
+        element(by.id('roundbutton')).click();
+        expect(browser.getTitle()).toEqual('IIFramework');
+    });
 });
