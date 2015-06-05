374fcb107a4fba4e62e7cae9060ce9ad388419d3
Atte Lassila <atte.lassila@cs.helsinki.fi>
2015-06-05 11:52:40 +0300

    removed test code from main

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/application/Application.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/application/Application.java
@@ -34,15 +34,6 @@ public class Application {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         logger.log(Level.CONFIG, "Following beans found:\t{0}", Arrays.toString(ctx.getBeanDefinitionNames()));
 
-        InformationSourceManager infSourceManager = ctx.getBean(InformationSourceManager.class);
-
-        InformationSourceConfiguration infSourceConfiguration = new InformationSourceConfiguration();
-        infSourceConfiguration.setName("Example Config");
-        infSourceConfiguration.setType(InformationSourceType.XML);
-        infSourceConfiguration.setUrl("http://axwikstr.users.cs.helsinki.fi/data.xml");
-        infSourceManager.createSource(infSourceConfiguration);
-        infSourceManager.getSources().get(0).readAndWrite();
-
         SysError e = new SysError(ErrorType.UNKNOWN_ERROR, ErrorSeverity.NOTIFICATION, "This is a test error");
         ErrorLogger.log(e);
 
