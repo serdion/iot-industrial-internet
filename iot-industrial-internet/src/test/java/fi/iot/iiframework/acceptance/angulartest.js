b837589fa1feba0524146956cb3730ad3a3d9964
Teemu Salminen <teemujsalminen@gmail.com>
2015-06-05 16:38:41 +0300

    improved the acceptance tests quite a bit and added a guide for using them as a comment

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/angulartest.js
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/angulartest.js
@@ -1,4 +1,13 @@
 describe('Acceptance testing for the AngularJS web interface', function () {
+    
+    // guide to using these acceptance tests:
+    // 1) install Protractor by typing "npm install -g protractor" to the command line
+    // 2) then type "webdriver-manager update" to the command line
+    // 3) now every time you want to run these tests, first type "webdriver-manager start" to start the test server,
+    // choose "Run Project" in NetBeans and then type "protractor conf.js" in the 
+    // cd iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/ folder
+    // 4) make sure to choose "Run Project" in Netbeans again before running the tests a second time
+    // or there will be errors in the tests
 
     it('the web interface should have a title', function () {
         browser.get('http://localhost:8080/#/');
@@ -8,7 +17,7 @@ describe('Acceptance testing for the AngularJS web interface', function () {
     it('should be able to move to the sources list by clicking the sidebar button', function () {
         browser.get('http://localhost:8080/#/');
         element(by.partialLinkText('Information Sources')).click();
-        expect(browser.getTitle()).toEqual('IIFramework');
+        expect(element.all(by.repeater('ds in sources')).count()).toEqual(1);
     });
     
     it('should be able view a sensor and filter its readouts', function () {
@@ -18,7 +27,7 @@ describe('Acceptance testing for the AngularJS web interface', function () {
         element(by.model("more")).sendKeys("22.50");
         element(by.model("less")).sendKeys("22.80");
         element(by.id('filter')).click();
-        expect(browser.getTitle()).toEqual('IIFramework');
+        expect(element.all(by.repeater('readout in readouts')).count()).toEqual(10);
     });
 
     it('should be able create a new information source', function () {
@@ -33,6 +42,7 @@ describe('Acceptance testing for the AngularJS web interface', function () {
         url.sendKeys('http://axwikstr.users.cs.helsinki.fi/data.xml');
         frequency.sendKeys('154');
         element(by.id('roundbutton')).click();
-        expect(browser.getTitle()).toEqual('IIFramework');
+        element(by.partialLinkText('Configurations')).click();
+        expect(element.all(by.repeater('c in configurations')).count()).toEqual(2);
     });
 });
