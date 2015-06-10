9ece6a1bcc5cda379e5ae4a61929a3d8b57f9e13
Ooppa <OpaPosti@hotmail.com>
2015-06-10 14:38:02 +0300

    Removed wrong imports

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/security/SanitizationHelperTest.java
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/security/SanitizationHelperTest.java
@@ -6,8 +6,6 @@
  */
 package fi.iot.iiframework.security;
 
-import ch.qos.logback.core.spi.ScanException;
-import com.sun.xml.internal.ws.policy.PolicyException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -22,11 +20,12 @@ public class SanitizationHelperTest {
     private static final String sometimesAcceptedInputs[] = {
         "<b>Bold text</b>",
         "<i>Italic text</i>",
-        "<a href=\"#\">link text</a>",
-    };
+        "<a href=\"#\">link text</a>",};
 
     private static final String testNotAcceptedInputs[] = {
         "<a>link text</a>",
+        "<a style=\"display: none;\">link text</a>",
+        "<div>link text</div>",
         "<SCRIPT type=\"text/javascript\">Default Script-tag String</SCRIPT>",
         "<body onload=alert('Noob attack')>",
         "<b onmouseover=alert('Wufff!')>More complex JS Attack</b>",
@@ -36,21 +35,21 @@ public class SanitizationHelperTest {
     }
 
     @Test
-    public void testSanitizeStrictWithAcceptedInputs() throws ScanException, PolicyException {
+    public void testSanitizeStrictWithAcceptedInputs() {
         for (int i = 0; i < alwaysAcceptedInputs.length; i++) {
             assertEquals(alwaysAcceptedInputs[i], SanitizationHelper.sanitizeStrict(alwaysAcceptedInputs[i]));
         }
     }
-    
+
     @Test
-    public void testSanitizeStrictWithNotAcceptedInputs() throws ScanException, PolicyException {
+    public void testSanitizeStrictWithNotAcceptedInputs() {
         for (int i = 0; i < testNotAcceptedInputs.length; i++) {
             assertNotEquals(testNotAcceptedInputs[i], SanitizationHelper.sanitizeStrict(testNotAcceptedInputs[i]));
         }
     }
-    
+
     @Test
-    public void testSanitizeLooseWithAcceptedInputs() throws ScanException, PolicyException {
+    public void testSanitizeLooseWithAcceptedInputs() {
         for (int i = 0; i < alwaysAcceptedInputs.length; i++) {
             assertEquals(alwaysAcceptedInputs[i], SanitizationHelper.sanitizeLoose(alwaysAcceptedInputs[i]));
         }
@@ -59,9 +58,9 @@ public class SanitizationHelperTest {
             assertEquals(sometimesAcceptedInputs[i], SanitizationHelper.sanitizeLoose(sometimesAcceptedInputs[i]));
         }
     }
-    
+
     @Test
-    public void testSanitizeLooseWithNotAcceptedInputs() throws ScanException, PolicyException {
+    public void testSanitizeLooseWithNotAcceptedInputs() {
         for (int i = 0; i < testNotAcceptedInputs.length; i++) {
             assertNotEquals(testNotAcceptedInputs[i], SanitizationHelper.sanitizeLoose(testNotAcceptedInputs[i]));
         }
