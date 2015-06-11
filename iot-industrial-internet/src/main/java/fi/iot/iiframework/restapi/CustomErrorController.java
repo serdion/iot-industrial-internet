9f2cd94b731de46e63cf13f225078c7267322d26
Ooppa <OpaPosti@hotmail.com>
2015-06-11 14:57:20 +0300

    Error logging for HTTP errors

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/restapi/CustomErrorController.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/restapi/CustomErrorController.java
@@ -6,15 +6,72 @@
  */
 package fi.iot.iiframework.restapi;
 
+import com.google.common.base.Throwables;
+import fi.iot.iiframework.errors.ErrorLogger;
+import fi.iot.iiframework.errors.ErrorSeverity;
+import fi.iot.iiframework.errors.ErrorType;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-public class CustomErrorController implements ErrorController {
+@Controller
+class CustomErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String customError(HttpServletRequest request, HttpServletResponse response, Model model) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+        String exceptionMessage = getExceptionMessage(throwable, statusCode);
+
+        // Log everything except 404 errors
+        logIfNot404Error(statusCode, exceptionMessage, throwable, getURI(request));
+        
+        model.addAttribute("exceptionMessage", exceptionMessage);
+        model.addAttribute("url", getURI(request));
+        model.addAttribute("statuscode", statusCode);
+        model.addAttribute("throwable", throwable);
+
+        return "errorpage";
+    }
     
-    
+    private void logIfNot404Error(Integer statusCode, String exceptionMessage, Throwable throwable, String uri){
+        String trace = "No stacktrace.";
+        
+        if(throwable!=null){
+            trace = throwable.getCause().toString();
+        }
+        
+        ErrorLogger.log(
+                ErrorType.HTTP_ERROR, 
+                ErrorSeverity.NOTIFICATION, 
+                "User experienced an error ("+ exceptionMessage +" - "+ statusCode + ") :"+ trace, 
+                "Occured at: "+ uri);
+    }
+
+    private String getURI(HttpServletRequest request) {
+        if (request.getAttribute("javax.servlet.error.request_uri") == null) {
+            return "Unknown URI";
+        }
+
+        return (String) request.getAttribute("javax.servlet.error.request_uri");
+    }
+
+    private String getExceptionMessage(Throwable throwable, Integer statusCode) {
+        if (throwable != null) {
+            return Throwables.getRootCause(throwable).getMessage();
+        }
+        HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
+        return httpStatus.getReasonPhrase();
+    }
 
     @Override
     public String getErrorPath() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return PATH;
     }
-    
 }
