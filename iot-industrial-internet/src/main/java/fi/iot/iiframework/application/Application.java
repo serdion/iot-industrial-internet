6169fea70db41966bd78907f7d48b4411ac17869
a544jh <axel.wikstrom@gmail.com>
2015-06-12 14:37:30 +0300

    changes for war

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/application/Application.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/application/Application.java
@@ -21,15 +21,22 @@ import java.util.logging.Logger;
 import javax.xml.bind.JAXBException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("fi.iot.iiframework")
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static final Logger logger = Logger.getLogger(Application.class.getName());
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
+    
     public static void main(String[] args) throws JAXBException, MalformedURLException, IOException {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         logger.log(Level.CONFIG, "Following beans found:\t{0}", Arrays.toString(ctx.getBeanDefinitionNames()));
