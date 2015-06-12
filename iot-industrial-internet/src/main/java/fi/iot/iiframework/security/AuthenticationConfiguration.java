5fef7d81cfd23c5da3292e988fdf8d14beb91c98
Ooppa <OpaPosti@hotmail.com>
2015-06-12 15:01:26 +0300

    Now it somewhat works...

--- iot-industrial-internet/src/main/java/fi/iot/iiframework/security/AuthenticationConfiguration.java
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/security/AuthenticationConfiguration.java
@@ -9,20 +9,18 @@ package fi.iot.iiframework.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebMvcSecurity
 public class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
 
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
 
         // From example, no idea how this actually works
-        auth.ldapAuthentication()
-                .userDnPatterns("uid={0},ou=people")
-                .groupSearchBase("ou=groups")
-                .contextSource().ldif("classpath:test-server.ldif");
+        /*        auth.ldapAuthentication()
+         * .userDnPatterns("uid={0},ou=people")
+         * .groupSearchBase("ou=groups")
+         * .contextSource().ldif("classpath:test-server.ldif");*/
 
         // Test Users
         auth.inMemoryAuthentication()
