2d8df21ba60e4dff6aa6973dd1bcbcbb822e4af7
Teemu Salminen <teemujsalminen@gmail.com>
2015-06-15 13:45:29 +0300

    date/frequency form should now work as it should?

---
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/parsers/SparkFunDataParserTest.java
@@ -0,0 +1,58 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+package fi.iot.iiframework.parsers;
+
+import fi.iot.iiframework.domain.Sensor;
+import java.util.Collection;
+import java.util.List;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ *
+ * @author atte
+ */
+public class SparkFunDataParserTest {
+
+    List<Sensor> sensors;
+
+    @Before
+    public void setUp() {
+        sensors = SparkfunDataParser.parse("https://data.sparkfun.com/output/dZ4EVmE8yGCRGx5XRX1W.json?page=1");
+    }
+
+    @Test
+    public void sparkFunDataParserFindsSensors() {
+        sensors.contains(new Sensor("humidity"));
+        sensors.contains(new Sensor("dailyrainin"));
+        sensors.contains(new Sensor("tempf"));
+    }
+
+    @Test
+    public void sensorsGetReadouts() {
+        Sensor sensor = sensors.get(0);
+        assertTrue(sensor.getReadouts().size() > 10);
+    }
+    
+    @Test
+    public void readoutsHaveValueSet() {
+        Sensor sensor = sensors.get(0);
+        sensor.getReadouts().forEach(r -> {
+            assertTrue(r.getValue() != 0);
+        });
+    }
+
+    @Test
+    public void readoutsHaveTimeSet() {
+        Sensor sensor = sensors.get(0);
+        sensor.getReadouts().forEach(r -> {
+            assertTrue(r.getTime() != 0);
+        });
+    }
+}
