b0d09746b4a6d75068a30e7073289603f0b6de51
Axel Wikström <axel.wikstrom@gmail.com>
2015-06-17 15:45:27 +0300

    modify init script for war

--- iot-industrial-internet/startup.sh
+++ iot-industrial-internet/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-exec nohup java -Dserver.port=8083 -Dserver.contextPath=/iot-master -jar target/iiframework-0.0.1-DEV.jar < /dev/null 2>> ../logs/errorlog >> ../logs/log
+exec nohup java -Dserver.port=8083 -Dserver.contextPath=/iot-master -jar target/iiframework.war < /dev/null 2>> ../logs/errorlog >> ../logs/log
 
