0b9ae99413791f1d4d597dd0aa37d8f0e90a7010
Ooppa <OpaPosti@hotmail.com>
2015-06-17 13:16:18 +0300

    FIle was not added

---
+++ iot-industrial-internet/src/main/java/fi/iot/iiframework/restapi/exceptions/TooManyRequestsException.java
@@ -0,0 +1,26 @@
+/*
+ * IoT - Industrial Internet Framework
+ * Apache License Version 2.0, January 2004
+ * Released as a part of Helsinki University
+ * Software Engineering Lab in summer 2015
+ */
+package fi.iot.iiframework.restapi.exceptions;
+
+public class TooManyRequestsException extends Exception {
+
+    public TooManyRequestsException() {
+    }
+
+    public TooManyRequestsException(String message) {
+        super(message);
+    }
+
+    public TooManyRequestsException(Throwable cause) {
+        super(cause);
+    }
+
+    public TooManyRequestsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
