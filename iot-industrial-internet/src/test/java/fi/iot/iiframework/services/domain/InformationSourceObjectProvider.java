7ae35d2c1cf609c2ea2c856fe0abfed8fcd6cf06
Atte Lassila <atte.lassila@cs.helsinki.fi>
2015-06-17 14:28:23 +0300

    fixed tests

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/services/domain/InformationSourceObjectProvider.java
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/services/domain/InformationSourceObjectProvider.java
@@ -25,8 +25,8 @@ import java.util.UUID;
  */
 public class InformationSourceObjectProvider {
     
-    public static List<Sensor> provideSensorsWithChildren(InformationSource parent) {
-        List<Sensor> sensors = new ArrayList<>();
+    public static Set<Sensor> provideSensorsWithChildren(InformationSource parent) {
+        Set<Sensor> sensors = new HashSet<>();
         for (int i = 0; i < 3; i++) {
             Sensor s = provideSensor();
             s.setSource(parent);
@@ -35,7 +35,7 @@ public class InformationSourceObjectProvider {
             }
             sensors.add(s);
         }
-        parent.setSensors((Set<Sensor>) sensors);
+        parent.setSensors(sensors);
         return sensors;
     }
 
