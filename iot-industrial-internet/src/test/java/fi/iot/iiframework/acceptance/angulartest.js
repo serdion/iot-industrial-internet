b0b3941d924585b82501a43ef7e2ad6de36fcfa7
Teemu Salminen <teemujsalminen@gmail.com>
2015-06-24 13:32:49 +0300

    protractor tests somewhat closer to being finished

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/angulartest.js
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/angulartest.js
@@ -7,7 +7,7 @@ describe('Acceptance testing for the AngularJS web interface', function () {
     // choose "Run Project" in NetBeans and then type "protractor conf.js" in the 
     // cd iot-industrial-internet/src/test/java/fi/iot/iiframework/acceptance/ folder
     // 4) make sure to choose "Run Project" in Netbeans again before running the tests a second time
-    // or there will be errors in the tests
+    // or there can/will be errors in the tests
 
     it('login page should have a title', function () {
         browser.driver.get('http://localhost:8080/login');
@@ -39,55 +39,62 @@ describe('Acceptance testing for the AngularJS web interface', function () {
         expect(element(by.partialLinkText('Add New')).isPresent()).toBe(true);
     });
 
-    it('should be able to move to the errors list, view an error and then return to the errors list', function () {
+    it('should be able to view occurred errors', function () {
         browser.driver.get('http://localhost:8080/login');
         browser.driver.findElement(by.name('username')).sendKeys('moderator');
         browser.driver.findElement(by.name('password')).sendKeys('moderator');
         browser.driver.findElement(By.xpath("//button[contains(.,'Login')]")).click();
+        element(by.partialLinkText('Information Sources')).click();
+        element(by.partialLinkText('Read now')).click();
+        element(by.partialLinkText('Read now')).click();
         element(by.partialLinkText('Errors')).click();
         element(by.id('Details')).click();
         element(by.id('backbutton')).click();
         expect(element(by.id('Details')).isPresent()).toBe(true);
+        expect(element.all(by.repeater('error in errorlist')).count()).toEqual(1);
     });
-
-//    it('should be able view a sensor and filter its readouts', function () {
-//        browser.driver.get('http://localhost:8080/login');
-//        browser.driver.findElement(by.name('username')).sendKeys('moderator');
-//        browser.driver.findElement(by.name('password')).sendKeys('moderator');
-//        browser.driver.findElement(By.xpath("//button[contains(.,'Login')]")).click();
-//        element(by.partialLinkText('View')).click();
-//        element(by.partialLinkText('List')).click();
-//        element(by.model("more")).sendKeys("22.50");
-//        element(by.model("less")).sendKeys("22.80");
-//        element(by.id('filter')).click();
-//        expect(element.all(by.repeater('readout in readouts')).count()).toEqual(4);
-//    });
-//
-//    it('should be able create a new information source', function () {
-//        browser.driver.get('http://localhost:8080/login');
-//        browser.driver.findElement(by.name('username')).sendKeys('moderator');
-//        browser.driver.findElement(by.name('password')).sendKeys('moderator');
-//        browser.driver.findElement(By.xpath("//button[contains(.,'Login')]")).click();
-//        element(by.partialLinkText('Add New')).click();
-//        var name = element(by.model("is.name"));
-//        var type = element(by.model("is.type"));
-//        var url = element(by.model("is.url"));
-//        var frequency = element(by.model("readFrequency_s"));
-//        name.sendKeys('NewAcceptanceTestSource');
-//        element(by.cssContainingText('option', 'XML')).click();
-//        url.sendKeys('http://axwikstr.users.cs.helsinki.fi/data.xml');
-//        frequency.sendKeys('15400');
-//        element(by.id('submitbutton')).click();
-//        element(by.partialLinkText('Configurations')).click();
-//        expect(element.all(by.repeater('c in configurations')).count()).toEqual(2);
-//    });
-
-    it('should be albe to view occured errors', function () {
-        // TODO tee virhe esim restapipyynnöllä ja katso onko virhe ilmoittanut
-    });
-
+    
     it('should be notified of unread errors in the topbar', function () {
-        // TODO lukematon virhe vaihtaa iconin classia
+        browser.driver.get('http://localhost:8080/login');
+        browser.driver.findElement(by.name('username')).sendKeys('moderator');
+        browser.driver.findElement(by.name('password')).sendKeys('moderator');
+        browser.driver.findElement(By.xpath("//button[contains(.,'Login')]")).click();
+        element(by.partialLinkText('Information Sources')).click();
+        element(by.partialLinkText('Read now')).click();
+        element(by.partialLinkText('Read now')).click();
+        browser.get("http://localhost:8080/#/sources");
+        expect(element(by.class('{unviewederrorsblink: unviewedErrors}')).isPresent()).toBe(true);
+    });
+
+    it('should be able view a sensor and filter its readouts', function () {
+        browser.driver.get('http://localhost:8080/login');
+        browser.driver.findElement(by.name('username')).sendKeys('moderator');
+        browser.driver.findElement(by.name('password')).sendKeys('moderator');
+        browser.driver.findElement(By.xpath("//button[contains(.,'Login')]")).click();
+        element(by.partialLinkText('Information Sources')).click();
+        element(by.partialLinkText('View')).click();
+        element(by.partialLinkText('List')).click();
+        element(by.model("more")).sendKeys("40.0");
+        element(by.model("less")).sendKeys("30.0");
+        element(by.id('filter')).click();
+        expect(element.all(by.repeater('readout in readouts')).count()).toEqual(0);
+    });
+
+    it('should be able create a new information source', function () {
+        browser.driver.get('http://localhost:8080/login');
+        browser.driver.findElement(by.name('username')).sendKeys('moderator');
+        browser.driver.findElement(by.name('password')).sendKeys('moderator');
+        browser.driver.findElement(By.xpath("//button[contains(.,'Login')]")).click();
+        element(by.partialLinkText('Information Sources')).click();
+        element(by.partialLinkText('Add New')).click();
+        var name = element(by.model("is.name"));
+        var type = element(by.model("is.type"));
+        var url = element(by.model("is.url"));
+        name.sendKeys('NewAcceptanceTestSource');
+        element(by.cssContainingText('option', 'JSON')).click();
+        url.sendKeys('https://data.sparkfun.com/output/dZ4EVmE8yGCRGx5XRX1W.json?page=1');
+        element(by.id('submitbutton')).click();
+        expect(element.all(by.repeater('c in configurations')).count()).toEqual(2); //???
     });
 
 });
