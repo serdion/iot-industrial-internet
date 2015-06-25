f3dabb444c6dc4035dcee5793049e8635e711f2b
Petri Pihlajaniemi <petri.pihlajaniemi@gmail.com>
2015-06-25 10:18:58 +0300

    infosourcetest

--- iot-industrial-internet/src/test/java/fi/iot/iiframework/restapi/InformationSourceControllerTest.java
+++ iot-industrial-internet/src/test/java/fi/iot/iiframework/restapi/InformationSourceControllerTest.java
@@ -6,32 +6,43 @@
  */
 package fi.iot.iiframework.restapi;
 
+import fi.iot.iiframework.application.TestConfig;
 import fi.iot.iiframework.domain.InformationSource;
 import fi.iot.iiframework.domain.Readout;
 import fi.iot.iiframework.domain.Sensor;
 import fi.iot.iiframework.restapi.exceptions.InvalidObjectException;
 import fi.iot.iiframework.restapi.exceptions.InvalidParametersException;
 import fi.iot.iiframework.restapi.exceptions.ResourceNotFoundException;
+import fi.iot.iiframework.restapi.exceptions.TooManyRequestsException;
 import fi.iot.iiframework.services.domain.InformationSourceObjectProvider;
 import fi.iot.iiframework.services.domain.InformationSourceService;
 import fi.iot.iiframework.source.InformationSourceManager;
+import java.util.Date;
 import java.util.HashSet;
 import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {TestConfig.class})
 public class InformationSourceControllerTest {
 
     private InformationSourceController controller;
-    
+
     private InformationSource sourceA;
     private InformationSource sourceB;
 
@@ -41,43 +52,42 @@ public class InformationSourceControllerTest {
     private Readout readoutA;
     private Readout readoutB;
     private Readout readoutC;
-    
+
     @Mock
     private InformationSourceService sourceService;
-    
+
     @Mock
     private InformationSourceManager sourceManager;
-    
+
     @Mock
     private RestAPIHelper helper;
-    
+
     @Before
     public void setUp() throws ResourceNotFoundException {
         controller = new InformationSourceController();
         MockitoAnnotations.initMocks(this);
-        
+
         initLogin();
         initContext();
-        
+
         controller.setInformationSourceService(sourceService);
         controller.setInformationSourceManager(sourceManager);
         controller.setRestAPIHelper(helper);
 
-        
         when(helper.returnOrException(Matchers.eq(sourceA))).thenReturn(sourceA);
         when(helper.returnOrException(Matchers.eq(sourceB))).thenReturn(sourceB);
-        
+
         when(sourceService.get(Matchers.eq(sourceA.getId()))).thenReturn(sourceA);
         when(sourceService.get(Matchers.eq(sourceB.getId()))).thenReturn(sourceB);
     }
-    
+
     /*
-    Initiates the context in which the tests are run in.
-    */
-    private void initContext(){
+     Initiates the context in which the tests are run in.
+     */
+    private void initContext() {
         sourceA = new InformationSource();
         sourceA.setId(1l);
-        
+
         sourceB = new InformationSource();
         sourceB.setId(2l);
 
@@ -98,32 +108,32 @@ public class InformationSourceControllerTest {
 
         sourceA.addSensor(sensorA);
         sourceB.addSensor(sensorB);
-        
+
         sourceService.save(sourceA);
         // Don't save this sourceService.save(sourceB);
     }
 
     /*
-    Login as a moderator, broken if the in-memory logins are changed!
-    */
-    private void initLogin(){
+     Login as a moderator, broken if the in-memory logins are changed!
+     */
+    private void initLogin() {
         SecurityContextHolder.getContext().setAuthentication(
                 new UsernamePasswordAuthenticationToken("moderator", "moderator")
         );
     }
-    
+
     @After
     public void tearDown() {
     }
 
     @Test
-    public void testGetInformationSource() throws InvalidParametersException, ResourceNotFoundException  {
+    public void testGetInformationSource() throws InvalidParametersException, ResourceNotFoundException {
         InformationSource foundSource = controller.getInformationSource(1, null);
         assertEquals(foundSource, sourceA);
     }
 
     @Test
-    public void testAddInformationSource() throws InvalidParametersException, ResourceNotFoundException, InvalidObjectException  {
+    public void testAddInformationSource() throws InvalidParametersException, ResourceNotFoundException, InvalidObjectException {
         controller.addInformationSource(sourceB, null);
         InformationSource foundSource = controller.getInformationSource(2, null);
         assertEquals(foundSource, sourceB);
@@ -136,26 +146,36 @@ public class InformationSourceControllerTest {
 //        
 //        Mockito.verify(sourceManager, Mockito.times(1)).updateSource(sourceA);
 //    }
-
     @Test
-    public void testDeleteInformationSource() throws InvalidParametersException, ResourceNotFoundException  {
+    public void testDeleteInformationSource() throws InvalidParametersException, ResourceNotFoundException {
         controller.deleteInformationSource(1, null);
         Mockito.verify(sourceManager, Mockito.times(1)).removeSource(1);
     }
 
     @Test
-    public void testListInformationSourcesList() throws InvalidParametersException  {
-        assertFalse(controller.listInformationSourcesList(null)==null);
+    public void testListInformationSourcesList() throws InvalidParametersException {
+        assertFalse(controller.listInformationSourcesList(null) == null);
     }
 
     @Test
-    public void testListInformationSourcesListAmount() throws InvalidParametersException  {
-        assertFalse(controller.listInformationSourcesListAmount(1, null)==null);
+    public void testListInformationSourcesListAmount() throws InvalidParametersException {
+        assertFalse(controller.listInformationSourcesListAmount(1, null) == null);
     }
 
     @Test
-    public void testListInformationSourcesListFromTo() throws InvalidParametersException  {
-        assertFalse(controller.listInformationSourcesListFromTo(0, 1, null)==null);
+    public void testListInformationSourcesListFromTo() throws InvalidParametersException {
+        assertFalse(controller.listInformationSourcesListFromTo(0, 1, null) == null);
     }
-    
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void spammingReadInformationSourceThrowsTooManyRequestsException() throws InvalidParametersException, TooManyRequestsException {
+        ResponseEntity<SuccessObject> response = controller.readInformationSource(sourceB.getId());
+        System.out.println(response);
+        exception.expect(TooManyRequestsException.class);
+        response = controller.readInformationSource(sourceB.getId());
+    }
+
 }
